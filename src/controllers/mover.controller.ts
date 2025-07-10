89d06e98d09a5d063d4a5afb817109ef8165c3ea
jyeon03 <bjlyeon2003@naver.com>
2025-07-10 15:59:56 +0900

    [#35] fix: 에러 핸들링 미들웨어 적용 및 주석 정리

--- src/controllers/mover.controller.ts
+++ src/controllers/mover.controller.ts
@@ -4,6 +4,8 @@ import {
   fetchBookmarkedMovers,
 } from "../services/mover.service";
 import { IMoverListFilter } from "../types/mover.types";
+import { NotFoundError, AuthenticationError } from "../types/commonError";
+import { handleError } from "../utils/handleError";
 
 /**
  * 기사님 리스트 조회 (필터, 정렬, 키워드)
@@ -26,9 +28,14 @@ export const getMoverListController = async (
     };
 
     const movers = await fetchMoverList(filter);
+
+    if (!movers || movers.length === 0) {
+      throw new NotFoundError("조회된 기사님이 없습니다.");
+    }
+
     res.json({ success: true, data: movers });
   } catch (err) {
-    next(err);
+    handleError(res, err);
   }
 };
 
@@ -44,9 +51,17 @@ export const getBookmarkedMoversController = async (
     // TODO: 실제 인증된 사용자 ID를 가져오는 로직으로 변경 필요
     const userId = 1;
 
+    // 인증 실패 (추후 미들웨어 적용 시)
+    // if (!userId) throw new AuthenticationError("인증이 필요합니다.");
+
     const bookmarkedMovers = await fetchBookmarkedMovers(userId);
+
+    if (!bookmarkedMovers || bookmarkedMovers.length === 0) {
+      throw new NotFoundError("찜한 기사님이 없습니다.");
+    }
+
     res.json({ success: true, data: bookmarkedMovers });
   } catch (err) {
-    next(err);
+    handleError(res, err);
   }
 };
