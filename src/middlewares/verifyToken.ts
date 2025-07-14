8dfb45476076bed41895fbe312bfebb85c05cc77
gksktl111 <gksktl111@naver.com>
2025-07-14 18:41:34 +0900

    [#23] feat : 유저 정보 조회 액세스 토큰 전달 로직 변경

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -6,7 +6,7 @@ export const verifyAccessToken = (
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.cookies?.accessToken;
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "로그인이 필요합니다." });
