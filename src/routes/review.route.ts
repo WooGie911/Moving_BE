af0788abb77b0c6e96767a929aeda89ca2bb0731
rlawodnr1109 <wodnr1109@naver.com>
2025-07-15 20:53:45 +0900

    [#74] refactor: API 명세서에 맞게 수정.

--- src/routes/review.route.ts
+++ src/routes/review.route.ts
@@ -4,15 +4,15 @@ import reviewController from "../controllers/review.controller";
 const reviewRouter = Router();
 
 // 리뷰 작성 (완료 처리)
-reviewRouter.patch("/:id", reviewController.postReview);
+reviewRouter.patch("/:reviewId", reviewController.postReview);
 
 // 리뷰 작성 가능한 Quote 리스트
 reviewRouter.get("/writable-quotes", reviewController.getWritableQuotes);
 
 // 내가 쓴 리뷰 목록
-reviewRouter.get("/written", reviewController.getWrittenReviews);
+reviewRouter.get("/customer/:customerId", reviewController.getWrittenReviews);
 
 // 내가 받은 리뷰 목록
-reviewRouter.get("/received ", reviewController.getReceivedReviews);
+reviewRouter.get("/mover/:moverId ", reviewController.getReceivedReviews);
 
 export default reviewRouter;
