a0461792914dc73ed9a62b8c1447800318355555
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-07-21 18:21:20 +0900

    chore:푸시전 커밋

--- src/controllers/customerEstimateRequest.controller.ts
+++ src/controllers/customerEstimateRequest.controller.ts
@@ -188,6 +188,7 @@ const customerEstimateRequestController = {
   ): Promise<void> => {
     try {
       const userId = req.user?.userId;
+      const userRole = req.user?.userType;
       const estimateRequestId = req.query.estimateRequestId;
       const { message, moverId } = req.body;
       if (!userId || typeof userId !== "string") {
@@ -197,7 +198,13 @@ const customerEstimateRequestController = {
         });
         return;
       }
-
+      if (userRole !== "CUSTOMER") {
+        res.status(403).json({
+          success: false,
+          message: "현재 유저타입이 고객이 아닙니다.",
+        });
+        return;
+      }
       if (!estimateRequestId || typeof estimateRequestId !== "string") {
         res.status(400).json({
           success: false,
