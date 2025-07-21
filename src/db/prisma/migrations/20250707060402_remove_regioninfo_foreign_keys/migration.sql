a073da73f93d01231b8853e4e3ed1d00fad6a618
y10b <victory.jun01@gmail.com>
2025-07-21 15:06:28 +0900

    [#99]fix: 스키마 및 기본 셋업 수정

--- src/db/prisma/migrations/20250707060402_remove_regioninfo_foreign_keys/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "moving_requests" DROP CONSTRAINT "moving_requests_arrivalRegion_fkey";
-
--- DropForeignKey
-ALTER TABLE "moving_requests" DROP CONSTRAINT "moving_requests_departureRegion_fkey";
