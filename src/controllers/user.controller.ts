4dd3757543fcbf0187f03c39e57b87044be6989f
gksktl111 <gksktl111@naver.com>
2025-08-05 15:26:44 +0900

    fix : 프로필 등록시 provider 값에따라 쿠키 저장 방식을 분기처리

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -66,23 +66,35 @@ const postProfile = async (req: Request, res: Response) => {
         preferredServices: req.body.preferredServices,
       };
 
-      const { result, accessToken, refreshToken } = await createCustomerProfile(
-        userId,
-        profileData
-      );
+      const { result, accessToken, refreshToken, provider } =
+        await createCustomerProfile(userId, profileData);
 
-      res.cookie(
-        "refreshToken",
-        refreshToken,
-        authCookieOptions(TOKEN_EXPIRES.REFRESH_TOKEN_COOKIE)
-      );
+      if (provider === "LOCAL") {
+        res.cookie(
+          "refreshToken",
+          refreshToken,
+          authCookieOptions(TOKEN_EXPIRES.REFRESH_TOKEN_COOKIE)
+        );
 
-      res.json({
-        success: true,
-        message: PROFILE_SUCCESS_MESSAGES.CUSTOMER_PROFILE_CREATED,
-        data: result,
-        accessToken,
-      });
+        res.json({
+          success: true,
+          message: PROFILE_SUCCESS_MESSAGES.CUSTOMER_PROFILE_CREATED,
+          data: result,
+          accessToken,
+        });
+      } else {
+        res.cookie(
+          "accessToken",
+          accessToken,
+          authCookieOptions(TOKEN_EXPIRES.ACCESS_TOKEN_COOKIE)
+        );
+
+        res.cookie(
+          "refreshToken",
+          refreshToken,
+          authCookieOptions(TOKEN_EXPIRES.REFRESH_TOKEN_COOKIE)
+        );
+      }
     } else if (userType === "MOVER") {
       // 기사님 프로필 등록
       const profileData: TMoverProfileInput = {
